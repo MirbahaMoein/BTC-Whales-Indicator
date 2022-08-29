b266191b912f3439493fc6d6dc0834208fe246d9
MirbahaMoein <mirbahamoein98@gmail.com>
2022-08-29 10:29:13 +0430

    visualize

--- chart.py
+++ chart.py
@@ -5,69 +5,81 @@ from sklearn.linear_model import LinearRegression
 import numpy as np
 from datetime import datetime
 
+pd.options.mode.chained_assignment = None
+
 
 def connect_db():
-    global connection, cursor
     connection = pg.connect(
         "dbname = NURAFIN user = postgres password = NURAFIN")
     cursor = connection.cursor()
+    return (connection, cursor)
 
 
-def get_table():
-    connect_db()
-    table = cursor.execute(
-        "SELECT * FROM public.historicalbalance ORDER BY time ASC").fetchall()
+def get_table(cursor, mode):
+    if mode == 100 or mode == 500 or mode == 1000:
+        table = cursor.execute(
+            "SELECT time, totalbalance{}_btc, btcprice FROM public.historicalbalance ORDER BY time ASC".format(str(mode))).fetchall()
+        status = True
+    else:
+        table = []
+        status = False
+    df = pd.DataFrame(table, columns=['time', 'totalbalance_btc', 'btcprice'])
+    return df, status
+
+
+def add_regression(df):
+    timelist = np.array(df['time'].to_list()).reshape(-1, 1)
+    balancelist = np.array(df['totalbalance_btc'].to_list())
+    model = LinearRegression().fit(timelist, balancelist)
+    df['regression'] = model.predict(timelist)
+    return df
+
+
+def add_moving_averages(df, span):
+    df['slowbalancemovingaverage'] = df['totalbalance_btc'].ewm(
+        span=span).mean()
+    df['fastbalancemovingaverage'] = df['totalbalance_btc'].ewm(
+        span=span * 15).mean()
+    return df
+
+
+def convert_to_datetime(df):
+    for i in range(len(df)):
+        df['time'][i] = datetime.fromtimestamp(df['time'][i]/1000)
+    return df
+
+
+def show_chart(df):
+    (ax1, ax2) = plt.subplots(2)[1]
+
+    ax1.set_xlabel('timestamp')
+    ax1.set_ylabel('balance fast EMA - balance slow EMA', color='green')
+
+    ax1.set_ylabel('btc price', color='red')
+    ax1.set_yscale('log')
+
+    ax1.plot(df['time'], df['btcprice'], color='red')
+
+    ax2.plot(df['time'], df['fastbalancemovingaverage'] -
+             df['slowbalancemovingaverage'], color='green')
+    
+    ax2.plot(df['time'], [0] * len(df), color= 'gray')
+
+    ax1twin = ax1.twinx()
+    ax1twin.plot(df['time'], df['totalbalance_btc'], color='green')
+    plt.grid('x')
+    plt.show()
+
+
+def main():
+    (connection, cursor) = connect_db()
+    df, status = get_table(cursor, mode=1000)
     connection.close()
-    return table
+    if status:
+        df = add_regression(df)
+        df = add_moving_averages(df, 24)
+        df = convert_to_datetime(df)
+        show_chart(df)
 
 
-df = pd.DataFrame(
-    get_table(), columns=['time', 'totalbalance100_btc', 'totalbalance500_btc', 'totalbalance1000_btc' , 'btcprice'])
-
-timelist = np.array(df['time'].to_list()).reshape(-1, 1)
-
-balancelist100 = np.array(df['totalbalance100_btc'].to_list())
-balancelist500 = np.array(df['totalbalance500_btc'].to_list())
-balancelist1000 = np.array(df['totalbalance1000_btc'].to_list())
-
-model100 = LinearRegression().fit(timelist, balancelist100)
-model500 = LinearRegression().fit(timelist, balancelist500)
-model1000 = LinearRegression().fit(timelist, balancelist1000)
-
-regressionline100 = model100.predict(timelist)
-regressionline500 = model100.predict(timelist)
-regressionline1000 = model100.predict(timelist)
-
-df['regressionpoints100'] = regressionline100
-df['regressionpoints500'] = regressionline500
-df['regressionpoints1000'] = regressionline1000
-
-df['balance100movingaverage'] = df['totalbalance100_btc'].ewm(span=10).mean()
-df['balance500movingaverage'] = df['totalbalance500_btc'].ewm(span=10).mean()
-df['balance1000movingaverage'] = df['totalbalance1000_btc'].ewm(span=10).mean()
-
-for i in range(len(df)):
-    df['time'][i] = datetime.fromtimestamp(df['time'][i]/1000)
-
-fig, (ax1, ax2) = plt.subplots(2)
-
-ax1.set_xlabel('timestamp')
-ax1.set_ylabel('total balance (btc) - EMA', color='green')
-
-ax1twin = ax1.twinx()
-ax1twin.plot(df['time'], df['totalbalance100_btc'], color='green')
-ax1twin.plot(df['time'], df['totalbalance500_btc'], color='blue')
-ax1twin.plot(df['time'], df['totalbalance1000_btc'], color='yellow')
-
-ax1.plot(df['time'], df['btcprice'], color='red')
-
-ax2.plot(df['time'], df['totalbalance100_btc'] - df['balance100movingaverage'], color='green')
-ax2.plot(df['time'], df['totalbalance500_btc'] - df['balance500movingaverage'], color='blue')
-ax2.plot(df['time'], df['totalbalance1000_btc'] - df['balance1000movingaverage'], color='yellow')
-
-ax1.set_ylabel('btc price', color='red')
-ax1.set_yscale('log')
-
-plt.grid('x')
-
-plt.show()
+main()
