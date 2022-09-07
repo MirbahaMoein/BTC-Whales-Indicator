7c27c71c88d218b1f931ffdecbfc8faab884d6bc
MirbahaMoein <mirbahamoein98@gmail.com>
2022-09-07 10:45:20 +0430

    generating balances hourly

--- walletbalances.py
+++ walletbalances.py
@@ -10,17 +10,18 @@ def init_db():
 
 def fetch_wallets(cursor):
     wallets = cursor.execute(
-        "SELECT address FROM public.wallets ORDER BY rank ASC").fetchall()
+        "SELECT DISTINCT address FROM public.transactions").fetchall()
     return wallets
 
 def generate_balance_timeseries(wallets, connection, cursor):
+
     for wallet in tqdm(wallets, desc = 'Wallet', position = 0):
         
         address = wallet[0]
         txs = cursor.execute("SELECT time, balance_btc FROM public.transactions WHERE address = %s", (address,)).fetchall()
         
         firsttxtime = txs[0][0]
-        candlesbefore = cursor.execute("SELECT time FROM public.klines WHERE time < %s ORDER BY time ASC", (firsttxtime,)).fetchall()
+        candlesbefore = cursor.execute("SELECT time FROM public.klines WHERE (time < %s AND time %% 3600000 = 0) ORDER BY time ASC", (firsttxtime,)).fetchall()
         for itrtr in tqdm(range(len(candlesbefore)), desc = 'Candles before', position = 1, leave = False):
             candletime = candlesbefore[itrtr][0]
             cursor.execute("INSERT INTO public.historicalwalletbalance VALUES (%s, %s, 0)", (address, candletime))
@@ -33,7 +34,7 @@ def generate_balance_timeseries(wallets, connection, cursor):
             tx2 = txs[itrtr + 1]
             tx2time = tx2[0]
             
-            candles = cursor.execute("SELECT time FROM public.klines WHERE (time < %s AND time >= %s) ORDER BY time ASC", (tx2time, tx1time)).fetchall()
+            candles = cursor.execute("SELECT time FROM public.klines WHERE (time < %s AND time >= %s AND time %% 3600000 = 0) ORDER BY time ASC", (tx2time, tx1time)).fetchall()
             for citrtr in range(len(candles)):
                 candletime = candles[citrtr][0]
                 cursor.execute("INSERT INTO public.historicalwalletbalance VALUES (%s, %s, %s)", (address, candletime, tx1balance))
@@ -41,7 +42,7 @@ def generate_balance_timeseries(wallets, connection, cursor):
         lasttx = txs[-1]
         lasttxtime = lasttx[0]
         lasttxbalance = lasttx[1]
-        candlesafter = cursor.execute("SELECT time FROM public.klines WHERE time >= %s ORDER BY time ASC", (lasttxtime,)).fetchall()
+        candlesafter = cursor.execute("SELECT time FROM public.klines WHERE (time >= %s AND time %% 3600000 = 0) ORDER BY time ASC", (lasttxtime,)).fetchall()
         for itrtr in tqdm(range(len(candlesafter)), desc = 'Candles after', position = 1, leave = False):
             candletime = candlesafter[itrtr][0]
             cursor.execute("INSERT INTO public.historicalwalletbalance VALUES (%s, %s, %s)", (address, candletime, lasttxbalance))
