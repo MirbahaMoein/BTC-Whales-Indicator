5b7b7c87d3386827d65d7a44f64135a566ee0212
MirbahaMoein <mirbahamoein98@gmail.com>
2022-09-10 13:15:05 +0430

    Weekly timeframe

--- correlation/correlations.py
+++ correlation/correlations.py
@@ -14,20 +14,23 @@ def fetch_wallets(cursor):
     return cursor.execute("SELECT DISTINCT(address) FROM public.historicalwalletbalance").fetchall()
 
 
-def calculate_correlation(wallets, connection, cursor):
+def calculate_correlation(wallets, connection, cursor, timeframe):
+    klines = cursor.execute("SELECT time, close FROM public.klines WHERE MOD(time, %s) = 0", (timeframe,)).fetchall()
     for wallet in tqdm(wallets, desc= 'wallet', position= 0):
         walletdf = pd.DataFrame(columns = ['time', 'btc_price', 'balance'])
         address = wallet[0]
-        equalbalancetimespans = cursor.execute("SELECT starttime, endtime, balance_btc FROM public.historicalwalletbalance WHERE (address = %s)",(address,)).fetchall()
-        for timespan in tqdm(equalbalancetimespans, desc= 'timespan', position = 1, leave= False):
-            starttime = timespan[0]
-            endtime = timespan[1]
-            balance = timespan[2]
-            klines = cursor.execute("SELECT time, close FROM public.klines WHERE (time >= %s AND time <= %s AND MOD(time, 604800000) = 0)", (starttime, endtime)).fetchall()
-            for kline in tqdm(klines, desc= 'klines between', position = 2, leave= False):
-                timestamp = kline[0]
-                price = kline[1]
-                walletdf.loc[len(walletdf)] = {'time': timestamp, 'btc_price': price, 'balance': balance}
+        for kline in klines:
+            timestamp = kline[0]
+            btcprice = kline[1]
+            timespans = cursor.execute("SELECT balance_btc FROM public.historicalwalletbalance WHERE (starttime < %s AND endtime > %s AND address = %s)",(timestamp, timestamp, address)).fetchall()
+            if len(timespans) > 1:
+                print("kline in more than one timespan")
+                sys.exit()
+            elif len(timespans) == 0:
+                print("kline in no timespans")
+                sys.exit()                
+            else:
+                walletdf.loc[len(walletdf)] = {'time': timestamp, 'btc_price': btcprice, 'balance': timespans[0][0]}
         walletdf = walletdf.sort_values(by= 'time')
         correlation = walletdf['btc_price'].corr(walletdf['balance'])
         cursor.execute("UPDATE public.wallets SET balance_price_correlation = %s WHERE address = %s", (correlation, address))
@@ -37,7 +40,7 @@ def calculate_correlation(wallets, connection, cursor):
 def main():
     connection, cursor = connect_db()
     wallets = fetch_wallets(cursor)
-    calculate_correlation(wallets, connection, cursor)
+    calculate_correlation(wallets, connection, cursor, 604800000)
     connection.close()
 
 
--- correlation/walletbalances.py
+++ correlation/walletbalances.py
@@ -20,30 +20,25 @@ def fetch_wallets(cursor):
 
 def generate_balance_data(wallets, timeframe, connection, cursor):
     firstsavedtxtime = cursor.execute("SELECT MIN(time) FROM public.transactions").fetchall()[0][0]
-    firstsavedcandle = firstsavedtxtime - (firstsavedtxtime % timeframe) + timeframe
     lastklinesavedtime = cursor.execute("SELECT MAX(time) FROM public.klines").fetchall()[0][0]
     for wallet in tqdm(wallets, desc='Wallet', position=0):
         address = wallet[0]
         txs = cursor.execute(
             "SELECT time, balance_btc FROM public.transactions WHERE address = %s ORDER BY time ASC", (address,)).fetchall()
         firsttxtime = txs[0][0]
-        firsttxcandle = firsttxtime - (firsttxtime % timeframe)
-        cursor.execute("INSERT INTO public.historicalwalletbalance VALUES (%s, %s, %s, 0)", (address, firstsavedcandle, firsttxcandle))
+        cursor.execute("INSERT INTO public.historicalwalletbalance VALUES (%s, %s, %s, 0)", (address, firstsavedtxtime, firsttxtime-1))
         lasttxtime = txs[-1][0]
         lasttxbalance = txs[-1][1]
-        lasttxcandle = lasttxtime - (lasttxtime % timeframe) + timeframe
-        cursor.execute("INSERT INTO public.historicalwalletbalance VALUES (%s, %s, %s, %s)", (address, lasttxcandle, lastklinesavedtime, lasttxbalance))
+        cursor.execute("INSERT INTO public.historicalwalletbalance VALUES (%s, %s, %s, %s)", (address, lasttxtime, lastklinesavedtime, lasttxbalance))
         for itrtr in tqdm(range(len(txs)-1), desc='Transaction', position=1, leave=False):
             tx1 = txs[itrtr]
             tx1time = tx1[0]
-            firstcandletime = tx1time - (tx1time % timeframe) + timeframe
             tx1balance = tx1[1]
             tx2 = txs[itrtr + 1]
             tx2time = tx2[0]
-            lastcandletime = tx2time - (tx2time % timeframe) 
             try:
                 cursor.execute("INSERT INTO public.historicalwalletbalance VALUES (%s, %s, %s, %s)", (
-                    address, firstcandletime, lastcandletime, tx1balance))
+                    address, tx1time, tx2time-1, tx1balance))
             except:
                 cursor.execute("ROLLBACK")
         connection.commit()
