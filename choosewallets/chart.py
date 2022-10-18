370ec7ffd328bc7390f8c8819cec4cdc130886eb
MirbahaMoein <mirbahamoein98@gmail.com>
2022-10-18 13:19:51 +0330

    Feat: fast EMA minus slow EMA

--- choosewallets/chart.py
+++ choosewallets/chart.py
@@ -4,7 +4,7 @@ import pandas as pd
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
-def generate_totalbalance_chart(timeframe: int):
+def generate_totalbalance_charts(timeframe: int):
     credentials, dbname = read_db_credentials()
     connectioninfo = "dbname = {} ".format(dbname) + credentials
     with pg.connect(connectioninfo) as connection:
@@ -19,7 +19,34 @@ def generate_totalbalance_chart(timeframe: int):
                 "SELECT SUM(balance_btc) FROM public.historicalwalletbalance WHERE (starttime <= %s AND endtime >= %s AND address IN (SELECT address FROM public.wallets WHERE (balance_price_correlation > 0 AND balance_price_correlation != 'NaN')))", (timestamp, timestamp)).fetchall()[0][0]
             new_row = pd.Series({'time': timestamp, 'total_balance': totalbalance, 'btc_price': btcprice})
             df = pd.concat([df, new_row.to_frame().T], ignore_index= True)
-    df.plot(x= 'time', y= ['btc_price', 'total_balance'] , use_index= False, logy= True, secondary_y= 'total_balance')
+    
+    df['time'] = pd.to_datetime(df['time'], unit= 'ms')
+    
+    plt.figure()
+    
+    plt.subplot(311)
+    plt.plot(df['time'], df['btc_price'])
+    plt.yscale('log')
+    plt.title('Price')
+    plt.grid(True)
+
+    plt.subplot(312)
+    plt.plot(df['time'], df['total_balance'])
+    plt.yscale('log')
+    plt.title('Total Balance')
+    plt.grid(True)
+
+    df['level0'] = 0
+    df['level1'] = 25000
+
+    plt.subplot(313)
+    plt.plot(df['time'], df['total_balance'].ewm(span= 7).mean() - df['total_balance'].ewm(span = 28).mean())
+    plt.plot(df['time'], df['level0'], color= 'black')
+    plt.plot(df['time'], df['level1'], color= 'black')
+    plt.yscale('linear')
+    plt.title('Total Balance Trend')
+    plt.grid(True)
+
     plt.show()
 
-generate_totalbalance_chart(24*60*60*1000)
+generate_totalbalance_charts(24*60*60*1000)
