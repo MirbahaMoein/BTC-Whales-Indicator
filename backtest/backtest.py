bfbde43ddc1e7a3734b44ecc06ad4d825ac2b0ce
MirbahaMoein <mirbahamoein98@gmail.com>
2022-11-08 08:40:36 +0330

    Feat: read feather files

--- backtest/backtest.py
+++ backtest/backtest.py
@@ -2,14 +2,13 @@ import pandas as pd
 from pathlib import Path
 import os
 
-scriptpath = Path("./")
-folderpath = scriptpath.parent.absolute()
-fileslist = os.listdir(folderpath)
-filestoread = []
-for filename in fileslist:
-    if len(filename.split(".")) == 2:
-        extension = filename.split(".")[1]
-        print(extension)
-        if extension == "ftr":
-            df = pd.read_feather(str(folderpath) + "\\" + filename)
-            
+def readfiles():
+    scriptpath = Path("./")
+    mainfolderpath = scriptpath.parent.absolute()
+    datafolderpath = mainfolderpath / "indicatordatasets"
+    fileslist = os.listdir(datafolderpath)
+    filesread = {}
+    for filename in fileslist:
+        df = pd.read_feather(str(datafolderpath) + "\\" + filename)
+        filesread[filename.split(".")[0]] = df
+    return filesread
