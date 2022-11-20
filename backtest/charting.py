6422d6cf18eb6f8fd64ff73f639345bab65a2a22
MirbahaMoein <mirbahamoein98@gmail.com>
2022-11-20 13:10:13 +0330

    Feat: shows signals

--- backtest/charting.py
+++ backtest/charting.py
@@ -4,6 +4,7 @@ import pandas as pd
 from pathlib import Path
 import re
 
+
 def readfiles() -> dict:
     scriptpath = Path("./")
     mainfolderpath = scriptpath.parent.absolute()
@@ -25,17 +26,33 @@ for filename in list(files):
         lowerband = float(re.findall('[^,]+(?=' + ',' + str(int(higherband)) + '-dataframe)', filename)[0])
     df = files[filename]
     df['time'] = pd.to_datetime(df['time']*1000000)
+
+    longsignals = {'x': [], 'y': []}
+    shortsignals = {'x': [], 'y': []}
+    for index in range(1, len(df)):
+        if df['position'][index] != df['position'][index - 1]:
+            x = df['time'][index]
+            y = df['close'][index]
+            if df['position'][index] > 0:
+                longsignals['x'].append(x)
+                longsignals['y'].append(y)
+            elif df['position'][index] < 0:
+                shortsignals['x'].append(x)
+                shortsignals['y'].append(y)
+
     
     plt.figure()
 
     plt.subplot(211)
-    plt.plot(df['time'], df['close'])
+    plt.plot(df['time'], df['close'], color= 'gray')
+    plt.scatter(x= longsignals['x'], y= longsignals['y'], s= 50, marker= '^', c= 'g')
+    plt.scatter(x= shortsignals['x'], y= shortsignals['y'], s= 50, marker= 'v', c= 'r')
     #plt.yscale('log')
     plt.title('Price')
     plt.grid(True)
 
     plt.subplot(212)
-    plt.plot(df['time'], df['balance'])
+    plt.plot(df['time'], df['balance'], color= 'gray')
     #plt.yscale('log')
     plt.title('Portfolio balance')
     plt.grid(True)
